4dd3f0b416c8677cdf251107744f2dfda2932595
viralinkoficial <vivianeferreiracaroline@gmail.com>
2026-09-04 08:39:49 -0300

    Adicionar service worker do Modo Poder

---
+++ modo-poder/sw.js
@@ -0,0 +1,7 @@
+const CACHE="modo-poder-v1";
+const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.json","./icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
